559c99e5b02a812d99e318233c753d5e8b4942f6
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-07-20 11:31:14 -0400

    Create count-reads.sh

---
+++ pipeline/count-reads.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -o count-reads.out
+#SBATCH -e count-reads.err
+#SBATCH -p scavenger
+# usage: sbatch count-reads-qiime.sh /path/to/qiime /path/to/metabarcoding.sif 
+wd=$PWD
+cd $1
+for f in [123]*.qzv; do
+     unzip $f -d ${f%.qzv}
+done
+unzip 4_denoised-stats.qzv -d 4_denoised-stats
+cd $wd
+singularity exec --bind $1,$PWD $2 Rscript count-reads.R $1
