67775834b055362a017e771388fd34449749b7a2
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-07-21 13:35:04 -0400

    fix file path

--- pipeline/demux-barcodes.sh
+++ pipeline/demux-barcodes.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 #SBATCH --job-name=demux-barcode
 #SBATCH --mem=20000
-#SBATCH --partition scavenger 
+#SBATCH --partition scavenger
 #SBATCH --out=demux-barcode-%j.out
 #SBATCH --error=demux-barcode-%j.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
 # Usage: demux-barcode.sh /miniseq-dir /path/to/samplesheetname /path/to/metabarcoding.sif
-# Example: sbatch demux-barcode.sh path/to/miniseq-dir /path/to/samplesheet.csv 
-wd=$PWD
-cd $1/..
-datadir=$PWD
-now=$(date +'%Y%m%d') # set output directory name to current date
-outdir=$now'_results'
-mkdir $outdir
-mkdir $outdir/demultiplexed
+# Example: sbatch demux-barcode.sh path/to/miniseq-dir /path/to/samplesheet.csv /hpc/group/ldavidlab/metabarcoding.sif
 
-# Demultiplex
-singularity exec --bind $datadir $3 bcl2fastq -o $outdir/demultiplexed --interop-dir InterOp/$now --stats-dir Stats/$now --reports-dir Reports/$now --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --sample-sheet $2
+
+codedir=$PWD
+cd $1
+cd ..
+parent=$PWD
+now=$(date +'%Y%m%d')
+resFolder=$now'_results'
+outdir=$parent/$resFolder
+mkdir $outdir
+
+cd $1
+
+singularity exec --bind $parent $2 bcl2fastq -o $outdir --interop-dir InterOp/$now --stats-dir Stats/$now --reports-dir Reports/$now --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --sample-sheet $parent/$3
+
+# Clean up directory structure
+mkdir $outdir/demultiplexed
+mv $outdir/*.fastq.gz $outdir/demultiplexed/
 
 # move .err and .out files
 mkdir $outdir/Reports
