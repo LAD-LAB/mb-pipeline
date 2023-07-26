07ba2ab977752cdf4b57e418a2cedd94d693dc47
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-07-26 13:50:38 -0400

    Update count-reads.R

--- pipeline/count-reads.R
+++ pipeline/count-reads.R
@@ -35,7 +35,7 @@ for (i in seq_along(count.fs)) {
 }
 print('count table prior to renaming')
 head(count.files)
-if (length(colnames(data.frame(count.files))) !=8) {
+if (length(colnames(data.frame(count.files)))) !=8) {
   print(paste('Unexpected number of columns:' length(colnames(data.frame(count.files)))))
   print('Expected number of columns: 8')
   print('Are you missing output files from trnL-pipeline.sh? Are there extra directories in your qiime-dir?')
