a279774af0d600b04cbed5343873a7613737ab7e
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-08-25 10:54:01 -0400

    Update count-reads.sh

--- pipeline/count-reads.sh
+++ pipeline/count-reads.sh
@@ -2,7 +2,7 @@
 #SBATCH -o count-reads.out
 #SBATCH -e count-reads.err
 #SBATCH -p scavenger
-# usage: sbatch count-reads.sh /path/to/qiime /path/to/metabarcoding.sif 
+# usage: sbatch count-reads.sh /path/to/qiime /path/to/where-scripts-are-stored /path/to/metabarcoding.sif 
 wd=$PWD
 cd $1
 for f in [123]*.qzv; do
@@ -10,7 +10,7 @@ for f in [123]*.qzv; do
 done
 unzip 4_denoised-stats.qzv -d 4_denoised-stats
 cd $wd
-singularity exec --bind $1,$PWD $2 Rscript count-reads.R $1
+singularity exec --bind $1,$2,$PWD $3 Rscript $2/count-reads.R $1
 
 cd $1/..
 mv $wd/count-reads.out ./Reports
